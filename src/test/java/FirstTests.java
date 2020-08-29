53a423892d942fc33e0897d6ca89980c4f6faf93
unknown <uzytkownik@testGIT.com>
2020-08-29 14:32:39 +0200

    implmented podcast test

--- src/test/java/FirstTests.java
+++ src/test/java/FirstTests.java
@@ -5,32 +5,68 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
 
 public class FirstTests {
-
     WebDriver driver; //inicjalizacja pustej przeglądarki
     public void highlightElement(WebDriver driver, WebElement element){
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].setAttribute('style', 'background: green; border: 3px solid blue;');", element);
     }
-
-    @Before  //sekcja before, a właściwie metoda setup wykona się przed każdym testem
+    @Before //sekcja before, a właściwie metoda setup, wykona się przed każdym testem
     public void setup(){
-        System.setProperty("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe"); //1. wskazanie chrome.driver}
-        driver = new ChromeDriver(); //przypisanie do pustej przeglądarki(chrome) do wykonywania testów
+        System.setProperty("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe"); //1.wskazanie chrome.driver
+        driver = new ChromeDriver(); //przypisanie do pustej przeglądarki, przeglądarki chrome do wykonywania testów
         driver.manage().window().maximize();
         driver.get("https://dev.to");
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        //przed każdym kolejnym findElement/s poczekaj 10 sekund, zanim wywalisz error, co sekundę sprawdzaj czy element jest już dostępny
     }
-
     @Test
-    public void fistTest(){
-        driver.get("https://dev.to");
-        WebElement podcastBth = driver.findElement(By.xpath("//a[@href='/pod']"));
-        highlightElement(driver,podcastBth);
+    public void goToPodcastAndSelectThirdOneAndPlayIt(){
+        WebElement podcastBtn = driver.findElement(By.xpath("//a[@href='/pod']"));
+        highlightElement(driver,podcastBtn);
+        podcastBtn.click();
+        String podcastUrl = driver.getCurrentUrl();
+        //przejscie na inna strone z podcastami
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.urlToBe("https://dev.to/pod"));
+        //sprawdzaj co sekundę, czy url ma wartość https://dev/to/pod, jeśli w ciągu 10 sekund nie ma takiej wartości - wywal błąd
+        List<WebElement> podcasts = driver.findElements(By.tagName("h3"));
+//        for (WebElement podcast: podcasts) {
+//            highlightElement(driver,podcast);
+  //      }
+        WebElement thirdPodcast = podcasts.get(2);
+        thirdPodcast.click();
+        WebElement recordBtn = driver.findElement(By.id("record"));
+        recordBtn.click();
 
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.className("status-message"))));
+        String recordBtnClassAtrribute = recordBtn.getAttribute("class");
+
+        boolean isPlaying = recordBtnClassAtrribute.contains("playing");
+
+        assertTrue("podcast wasn't played",recordBtnClassAtrribute.contains("playing"));
     }
     @Test
     public void secondTest(){
-
     }
 }
+
+
